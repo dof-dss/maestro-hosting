ae2f3d9b78021e4804ad7224e42fad126f99df3d
OMAHM <hello@onemanandhismouse.com>
2024-10-25 19:52:44 +0100

    MAES-3 ~ Update paths to use absolute instead of problematic symlink

--- resources/unity/DDev/files/homeadditions/appserver_build.sh
+++ resources/unity/DDev/files/homeadditions/appserver_build.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 # Variables to indicate key settings files or directories for Drupal.
-DRUPAL_ROOT=/app/web
+DRUPAL_ROOT=/var/www/html/web
 DRUPAL_SETTINGS_FILE=$DRUPAL_ROOT/sites/default/settings.php
 DRUPAL_SERVICES_FILE=$DRUPAL_ROOT/sites/default/services.yml
 
 # If we don't have a Drupal install, download it.
-if [ ! -d "/app/web/core" ]; then
+if [ ! -d "/var/www/html/web/core" ]; then
   echo "Installing Drupal"
   export COMPOSER_PROCESS_TIMEOUT=600
   composer install
 fi
 
 # Create Drupal public files directory and set IO permissions.
-if [ ! -d "/app/web/files" ]; then
+if [ ! -d "/var/www/html/web/files" ]; then
   echo "Creating public Drupal files directory"
-  mkdir -p /app/web/files
-  chmod -R 0775 /app/web/files
+  mkdir -p /var/www/html/web/files
+  chmod -R 0775 /var/www/html/web/files
 fi
 
 # Create Drupal private file directory above web root.
-if [ ! -d "/app/private" ]; then
+if [ ! -d "/var/www/html/private" ]; then
   echo "Creating private Drupal files directory"
-  mkdir -p /app/private
+  mkdir -p /var/www/html/private
 fi
 
 if [ ! -d $DRUPAL_ROOT/sites/default/settings.local.php ]; then
   echo "Creating local Drupal settings and developent services files"
-  cp -v /app/.ddev/homeadditions/config/drupal.settings.php $DRUPAL_ROOT/sites/default/settings.local.php
-  cp -v /app/.ddev/homeadditions/config/drupal.services.yml $DRUPAL_ROOT/sites/local.development.services.yml
+  cp -v /var/www/html/.ddev/homeadditions/config/drupal.settings.php $DRUPAL_ROOT/sites/default/settings.local.php
+  cp -v /var/www/html/.ddev/homeadditions/config/drupal.services.yml $DRUPAL_ROOT/sites/local.development.services.yml
 fi
